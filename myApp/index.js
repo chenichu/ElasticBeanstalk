af0dec3627c06d1f1f906f79c522549d9ad56681
patrickc3832 <patrickc3832@gmail.com>
2020-05-26 11:48:08 +0200

    removed other code

--- myApp/index.js
+++ myApp/index.js
@@ -1,32 +1,11 @@
 const express = require('express');
 const app = express();
-const debug = require('debug')('myapp:server');
-const path = require('path');
-const multer = require('multer');
-const logger = require('morgan');
-const serveIndex = require('serve-index')
 var fs = require('fs');
 const uploadsDir = '/local/uploads';
-var http = require('http');
-
-// multer middleware handling form-data to handle 
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        //cb(null, './public/uploads')
-        cb(null, uploadsDir)
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-});
-
-//will be using this for uplading
-const upload = multer({ storage: storage });
 
 //get the router
 const userRouter =require('./routes/user.route');
 
-app.use(logger('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(express.static('public')); // Not used
@@ -52,14 +31,6 @@ app.get('/testFileRetrieval',function(req,res){
 app.get('/', function(req,res) {
     return res.send("hello from my app express server!")
 })
-
-//Testing file upload
-app.post('/testUpload', upload.single('file'), function(req,res) {
-    debug(req.file);
-    console.log('storage location is ', req.hostname +'/' + req.file.path);
-    return res.send(req.file);
-})
-
 //if end point is /users/, use the router.
 app.use('/users', userRouter);
 
